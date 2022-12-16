21766f0769847d956763eab94dd73c4e268e774a
unknown <you@example.com>
2022-12-16 14:35:26 +0000

    final deploy

---
+++ components/Categories.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect } from 'react'
+import moment from 'moment';
+import Link from 'next/link';
+
+import {getCategories} from '../services';
+
+const Categories = () => {
+    const [categories, setCategories] = useState([]); 
+
+    useEffect(() => {
+            getCategories()
+                .then((newCategories) => setCategories(newCategories))
+       }, []);
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+        <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            Categories
+        </h3>
+        {categories.map((category) => (
+            <Link href={`/category/${category.slug}`} key={category.slug} className ="text-md">
+            <span className='curor-pointer block pb-3 mb-3'>
+                {category.name}
+            </span>
+            
+        </Link>
+        ))}
+    </div>
+
+  )
+}
+
+export default Categories
