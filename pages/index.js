e6a391bf76c429bb181a2c0c9f044df345c358f9
unknown <you@example.com>
2022-12-16 21:55:16 +0000

    mydploy

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Crata_blog</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Blog Me</title>
+        <link rel="icon" href="/crata.jpg" />
       </Head>
     <FeaturedPosts/>
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
